1358293a8f9653466757148a90cce2ba6ec31043
kxrim <k.yagci@students.htl-leonding.ac.at>
2026-02-10 11:02:37 +0100

    auto create db when backend starts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,20 @@
 import express from "express";
 import cors from "cors";
 import router from "./routes/test";
+import {Unit} from "./db/unit";
 
+const PORT = 3000;
 const app = express();
+
+// Create db if not exists and ensure tables are created
+const unit = new Unit(true);
+unit.complete(true);
 
 app.use(cors());
 app.use(express.json());
 
 app.use("/api", router);
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log("Server is running on http://localhost:3000");
 });
