7db7ecf2b1c6baa727d3536a33ebc693ebf21f47
kxrim <k.yagci@students.htl-leonding.ac.at>
2026-02-10 11:09:10 +0100

    added templates to prevent workflow error

---
+++ frontend/app/pages/account.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-bold">Account</h1>
+    <p>This page is under construction.</p>
+  </div>
+</template>
+
---
+++ frontend/app/pages/logout.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <h1 class="text-2xl font-bold">Logout</h1>
+    <p>You have been logged out.</p>
+  </div>
+</template>
+
