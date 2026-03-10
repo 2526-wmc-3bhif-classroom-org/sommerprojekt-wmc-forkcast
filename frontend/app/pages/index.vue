6795427c30422249fac37af16fbbfd7913215cbb
nhaiderhtl <n.haider@students.htl-leonding.ac.at>
2026-03-10 09:13:30 +0100

    Update index.vue to enhance landing page with background image and updated call-to-action buttons

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -3,7 +3,9 @@
 <template>
 
   <section class="hero">
-    <div class="hero bg-base-200 min-h-screen">
+    <div class="hero bg-base-200 min-h-screen
+                bg-[linear-gradient(rgba(0,0,0,0), rgba(255,255,255,10)),url('../assets/images/hero-bg.jpg')]
+                bg-center">
       <div class="hero-content flex-col lg:flex-row">
         <div>
           <h1 class="text-6xl font-bold mb-2.5">Meet Forkcast,</h1>
@@ -11,8 +13,14 @@
           <p class="py-6 text-xl">
             Organize weekly meals, discover new recipes, and cook stress free.
           </p>
-          <NuxtLink to="/auth/register" class="btn btn-primary mr-5">Start Planning Free</NuxtLink>
-          <NuxtLink to="/auth/login" class="btn btn-outline">Login</NuxtLink>
+          <NuxtLink to="/auth/register" class="btn btn-primary mr-5">
+            <i class="fa-solid fa-user-plus mr-1"/>
+            Start Planning Free
+          </NuxtLink>
+          <NuxtLink to="/auth/login" class="btn btn-outline">
+            <i class="fa-solid fa-user-check mr-1"/>
+            Login
+          </NuxtLink>
         </div>
         <!--TODO add img/interactive of example meal plan-->
       </div>
