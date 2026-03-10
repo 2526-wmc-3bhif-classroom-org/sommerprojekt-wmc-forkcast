32fd1c2be7618019dfcb3459021cdd4f865d356b
nhaiderhtl <n.haider@students.htl-leonding.ac.at>
2026-03-10 08:32:15 +0100

    Update index.vue to enhance landing page with welcome message and login prompt

--- frontend/app/pages/index.vue
+++ frontend/app/pages/index.vue
@@ -1,3 +1,19 @@
 <template>
-  <p>Hello World!</p>
+
+  <section class="hero">
+    <div class="hero bg-base-200 min-h-screen">
+      <div class="hero-content flex-col lg:flex-row">
+        <!--TODO maybe img-->
+        <div>
+          <h1 class="text-5xl font-bold">Welcome To Forkcast!</h1>
+          <p class="py-6">
+            Your personal meal planner
+          </p>
+          <NuxtLink to="/login" class="btn btn-primary">Login To Get Started</NuxtLink>
+        </div>
+      </div>
+    </div>
+  </section>
+
+
 </template>
