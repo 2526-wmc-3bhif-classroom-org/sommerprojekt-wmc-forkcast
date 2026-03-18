5293ab7efc1b4832995431d241a3dd50ba43e4e3
e-reitbauer <erik.reitbauer2@gmail.com>
2026-03-18 19:26:42 +0100

    Some ui concepts

--- frontend/app/pages/dashboard/index.vue
+++ frontend/app/pages/dashboard/index.vue
@@ -1,3 +1,27 @@
+<script setup lang="ts">
+</script>
 <template>
-  <p>Hello logged in user</p>
+  <div class="hero min-h-screen">
+    <div class="hero-content text-center flex flex-col gap-10">
+      <h1 class="text-5xl font-bold ">What shall we cook today?</h1>
+
+      <div class="inline-flex space-x-20">
+        <recipe-card-component
+            image="https://img.spoonacular.com/recipes/716429-312x231.jpg"
+            title="Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs"
+            id="716429"
+            :popular="true"
+        />
+        <div class="divider divider-horizontal">OR</div>
+
+        <recipe-card-component
+            image="https://img.spoonacular.com/recipes/715538-312x231.jpg"
+            title="What to make for dinner tonight?? Bruschetta Style Pork & Pasta"
+            id="712429"
+            :popular="false"
+            :isAlternate="true"
+        />
+      </div>
+    </div>
+  </div>
 </template>
