bfabc2dd5e904b4bba4751e171344a10f856f07b
nhaiderhtl <n.haider@students.htl-leonding.ac.at>
2026-03-19 09:46:29 +0100

    Simplify privacy policy component by removing async data retrieval

--- frontend/app/pages/privacy-policy.vue
+++ frontend/app/pages/privacy-policy.vue
@@ -1,16 +1,3 @@
-<script setup lang="ts">
-const {data: md} = await useAsyncData('privacy-policy-md', async () => {
-  const {readFileSync} = await import('node:fs')
-  const {resolve} = await import('node:path')
-
-  let filePath = resolve(process.cwd(), 'app/content/privacy-policy.md')
-
-  return readFileSync(filePath, 'utf-8')
-})
-</script>
-
 <template>
-  <LegalPagesComponent>
-    <MDC class="prose lg:prose-xl" v-if="md" :value="md"/>
-  </LegalPagesComponent>
+  <LegalPagesComponent />
 </template>
