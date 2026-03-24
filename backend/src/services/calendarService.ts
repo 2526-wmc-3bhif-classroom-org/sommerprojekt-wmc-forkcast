4a6752b4bcea48deb6f19ab66d2961d4853ddedb
lGruenzweilHTL <lukas.gruenzweil@liwest.at>
2026-03-24 07:51:41 +0100

    calendar repo and service

---
+++ backend/src/services/calendarService.ts
@@ -0,0 +1,31 @@
+import { Unit } from "../db/unit";
+import { CalendarRepository } from "../repository/calendarRepository";
+import { CalendarEntry } from "../types";
+
+export class CalendarService {
+    private calendarRepository: CalendarRepository;
+
+    constructor(unit: Unit) {
+        this.calendarRepository = new CalendarRepository(unit);
+    }
+
+    public getCalendarEntries(userId: number): CalendarEntry[] {
+        return this.calendarRepository.findByUserId(userId);
+    }
+
+    public addCalendarEntry(userId: number, recipeId: number, date: Date): CalendarEntry {
+        return this.calendarRepository.create({
+            date,
+            userId,
+            recipeId
+        });
+    }
+
+    public removeCalendarEntry(userId: number, calendarEntryId: number): boolean {
+        const entry = this.calendarRepository.findById(calendarEntryId);
+        if (!entry || entry.userId !== userId) {
+            return false;
+        }
+        return this.calendarRepository.deleteById(calendarEntryId);
+    }
+}
