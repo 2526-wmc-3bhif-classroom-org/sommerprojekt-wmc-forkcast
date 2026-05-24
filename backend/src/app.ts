58431cdad0367261083cc972cefd80d398a7e2b3
lGruenzweilHTL <lukas.gruenzweil@liwest.at>
2026-05-24 17:22:38 +0200

    increase rate limit to 400/15min

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -43,7 +43,7 @@ app.use(cookieParser());
 app.use(express.json());
 app.use(rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    limit: 100, // Limit each IP to 100 requests per time window
+    limit: 400, // Limit each IP to 400 requests per time window
     standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
     ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
